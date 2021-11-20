62672a3ba12abd7721cb51bea19235cdb7dd1f90
Muhtasim Miraz <mmiraz@Muhtasims-MacBook-Air.local>
2021-11-20 01:54:46 -0500

    Assignment done

---
+++ Flix/SwiftUIViewhehe.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIViewhehe.swift
+//  Flix
+//
+//  Created by Muhtasim Miraz on 11/19/21.
+//
+
+import SwiftUI
+
+struct SwiftUIViewhehe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIViewhehe_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIViewhehe()
+    }
+}
