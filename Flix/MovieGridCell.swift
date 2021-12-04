22cf4e6ee78932683cd8f715a08a6c977829f0ee
Muhtasim Miraz <mmiraz@Muhtasims-MacBook-Air.local>
2021-12-04 01:50:26 -0500

    Part 2 completed

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Muhtasim Miraz on 12/4/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
